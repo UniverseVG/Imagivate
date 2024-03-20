c1c7676bcfba917fe8996e7252aab9ff1347dcaa
G M Varun <gmvarun@Gs-MacBook-Air.local>
2024-03-20 12:35:18 +0530

    implement all the features for imagivate

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks/clerk"],
-  // Routes that can be accessed while signed out
+  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
